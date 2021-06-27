e6f9de375c5fea967c8b853398bb3f3407e7782d
Tokyoyoyo <1278441739@qq.com>
2021-06-27 21:00:51 +0800

    say hello

---
+++ demo01-springInitializr/src/main/java/com/coder/demo01springinitializr/Demo01SpringInitializrApplication.java
@@ -0,0 +1,13 @@
+package com.coder.demo01springinitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo01SpringInitializrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo01SpringInitializrApplication.class, args);
+    }
+
+}
---
+++ demo01-springInitializr/src/test/java/com/coder/demo01springinitializr/Demo01SpringInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coder.demo01springinitializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo01SpringInitializrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
